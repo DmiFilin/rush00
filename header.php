b4514aba501570aa3ac82e00bd220cdb0ce8eec6
Gillam Hazrak zo loraq <ghazrak-@ox-q3.21-school.ru>
2019-05-05 21:22:56 +0300

    header

--- header.php
+++ header.php
@@ -15,24 +15,30 @@ session_start();
 			</div>
 		</div>
 		<div class="dropdown">
-            <?php
-            if ($_SESSION['loggued_on_user'] == NULL || $_SESSION['loggued_on_user'] == "")
-			    echo ('<a href="login.php"><button class="three">Sign in</button></a>');
-            else {
-                echo('<a href="logout.php"><button class="three">Logout</button></a>');
-            }
-            ?>
-		</div>
-		<div class="dropdown">
 			<a href="basket.php"><button class="four">Basket</button></a>
 		</div>
         <div class="dropdown">
-            <button class="two">Settings</button>
+			<?php
+			if ($_SESSION['loggued_on_user'] == NULL || $_SESSION['loggued_on_user'] == "")
+				echo ('<a href="login.php"><button class="three">Sign in</button></a>');
+			else {
+				echo('<a href="logout.php"><button class="three">Logout</button></a>');
+			}
+			?>
+        </div>
+        <div class="dropdown">
+			<?php
+			if ($_SESSION['loggued_on_user'] == NULL || $_SESSION['loggued_on_user'] == "")
+				echo ('<a href="create.php"><button class="three">Create account</button></a>');
+			else {
+				echo('<button class="two">Settings</button>
             <div class="dropdown-content">
                 <a href="modif.php">change password</a>
                 <a href="modif_login.php">change login</a>
                 <a href="delete_account.php">delete account</a>
-            </div>
+            </div>');
+			}
+			?>
         </div>
 	</div>
 </header>
