0d6fb660ba2b1f4f09cdba0972990d7f5b3ab238
Gillam Hazrak zo loraq <ghazrak-@ox-q3.21-school.ru>
2019-05-05 11:53:53 +0300

    header and css

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+	<div class="menu">
+		<div class="dropdown">
+			<a href="index.php"><button class="one">Main</button></a>
+		</div>
+		<div class="dropdown">
+			<button class="two">Categories</button>
+			<div class="dropdown-content">
+				<a href="#">T</a>
+				<a href="#">O</a>
+				<a href="#">P</a>
+			</div>
+		</div>
+		<div class="dropdown">
+			<a href="index.html"><button class="three">Sign in</button></a>
+		</div>
+		<div class="dropdown">
+			<a href="#"><button class="four">Basket</button></a>
+		</div>
+	</div>
+</header>
