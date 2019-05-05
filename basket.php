0050f67c03115a56f6aafea77fe127df00ccef85
Gillam Hazrak zo loraq <ghazrak-@ox-q3.21-school.ru>
2019-05-05 15:16:35 +0300

    basket

---
+++ basket.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Online-shop</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<?php require "header.php"; ?>
+<body>
+<br/>
+<br/>
+<form class="basket" action="" method="get">
+<table>
+	<h2>Products</h2>
+	<tr><td>Name</td><td>Price</td><td>Count</td><td>Total</td></tr>
+	<tr><td>Boots of Travel</td><td>1500</td><td>1</td><td> </td></tr>
+<!--		цикл-->
+	<tr><td> </td><td> </td><td></td><td>Здесь должна быть общая цена</td></tr>
+</table>
+	<br />
+	<input type="submit" name="buy" value="buy">
+</form>
+</body>
+</html>
